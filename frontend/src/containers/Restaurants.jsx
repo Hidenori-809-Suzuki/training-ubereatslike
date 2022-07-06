0ce4dbf1cb0e199b24b50f8504150f5dee9121a8
Hidenori_809 Suzuki <suzuki809hidenori@gmail.com>
2022-07-07 01:17:11 +0900

    section4-finish

---
+++ frontend/src/containers/Restaurants.jsx
@@ -0,0 +1,18 @@
+import React, { Fragment, useEffect } from 'react';
+
+// apis
+import { fetchRestaurants } from '../apis/restaurants'; //←この行を追加する
+
+export const Restaurants = () => {
+  useEffect(() => {
+    fetchRestaurants()
+    .then((data) =>
+      console.log(data)
+    )
+  }, [])
+  return (
+    <Fragment>
+      レストラン一覧
+    </Fragment>
+  )
+}
